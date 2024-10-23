e46ff2fc090519a904b9bf0aed35b67a63531d6c
bryanmcarlos <bryanmcarlos@gmail.com>
2024-10-23 11:46:05 +0400

    Update my.js

--- my.js
+++ my.js
@@ -27,8 +27,8 @@ function addGS() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            SSAccount: "TestAccount",
-            SSBookingID: "TestBooking",
+            Account: "TestAccount",
+            BookingID: "TestBooking",
             AmenityName: "testAmenityName",
             BookingDate: "testBookingDate",
             TimeSlot: "testTimeSlot",
