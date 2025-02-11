f11ac5b45d2b7a3b61083704262676aa5eab80c8
bryanmcarlos <bryanmcarlos@gmail.com>
2025-02-11 14:37:55 +0400

    Update my.js

--- my.js
+++ my.js
@@ -103,8 +103,8 @@ function get24HourTime(timeStr) {
 // Fixes the date picker issue ensuring it works with correct offsets
 document.getElementById("datePicker").addEventListener("change", function() {
     const selectedDate = new Date(this.value);
-    selectedDate = selectedDate - 1
-    const formattedDate = selectedDate.toISOString().split('T')[0]; // Ensure correct format
+    const adjSelectedDate = selectedDate - 1
+    const formattedDate = adjSelectedDate.toISOString().split('T')[0]; // Ensure correct format
     loadBryanBookings(formattedDate);
 });
 
